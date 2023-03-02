bb1e44ecd86fec13dea136b2811fbcf99ec64da5
Yash Garg <yash.garg.ext@murena.com>
2023-03-02 13:31:53 +0530

    feat(workspace): set font style to normal and decrease font spacing b/w icon

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -423,6 +423,8 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
             mLastOriginalText = label;
             mLastModifiedText = mLastOriginalText;
             mBreakPointsIntArray = StringMatcherUtility.getListOfBreakpoints(label, MATCHER);
+            setIncludeFontPadding(false);
+            setCompoundDrawablePadding(0);
             setText(label);
         }
         if (info.contentDescription != null) {
