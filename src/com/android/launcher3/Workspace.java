cc32746240186d94899f97ad9c9b22635711cc31
Yash Garg <yash.garg.ext@murena.com>
2023-03-21 16:04:43 +0530

    feat(status bar): hide status bar on widgets page

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -65,6 +65,7 @@ import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.android.launcher3.accessibility.AccessibleDragListenerAdapter;
 import com.android.launcher3.accessibility.WorkspaceAccessibilityHelper;
@@ -1371,12 +1372,15 @@ public class Workspace extends PagedView<WorkspacePageIndicatorDots>
 
             if (mCurrentPage != 0) {
                 mLauncher.mBlurLayer.setAlpha(0f);
+                getWindowInsetsController().show(WindowInsetsCompat.Type.statusBars());
             }
 
             if (mCurrentPage == 0 && prevPage == 1) {
                 navbarAnimator.start();
+                getWindowInsetsController().hide(WindowInsetsCompat.Type.statusBars());
             } else if (prevPage == 0 && mCurrentPage == 1) {
                 navbarAnimator.reverse();
+                getWindowInsetsController().show(WindowInsetsCompat.Type.statusBars());
             }
         }
     }
