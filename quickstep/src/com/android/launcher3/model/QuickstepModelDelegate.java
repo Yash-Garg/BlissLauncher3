5aa1b7b513c328c6029d83535a4b902442b05cd6
Yash Garg <yash.garg.ext@murena.com>
2023-03-22 20:48:38 +0530

    chore: initial setup for release builds

--- quickstep/src/com/android/launcher3/model/QuickstepModelDelegate.java
+++ quickstep/src/com/android/launcher3/model/QuickstepModelDelegate.java
@@ -48,6 +48,8 @@ import android.os.UserHandle;
 import android.util.Log;
 import android.util.StatsEvent;
 
+import androidx.annotation.Keep;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.WorkerThread;
 
@@ -307,7 +309,13 @@ public class QuickstepModelDelegate extends ModelDelegate {
     private void registerPredictor(PredictorState state, AppPredictor predictor) {
         state.predictor = predictor;
         state.predictor.registerPredictionUpdates(
-                MODEL_EXECUTOR, t -> handleUpdate(state, t));
+                MODEL_EXECUTOR, new AppPredictor.Callback() {
+                    @Keep
+                    @Override
+                    public void onTargetsAvailable(@NonNull List<AppTarget> targets) {
+                        handleUpdate(state, targets);
+                    }
+                });
         state.predictor.requestPredictionUpdate();
     }
 
@@ -322,13 +330,17 @@ public class QuickstepModelDelegate extends ModelDelegate {
     private void registerWidgetsPredictor(AppPredictor predictor) {
         mWidgetsRecommendationState.predictor = predictor;
         mWidgetsRecommendationState.predictor.registerPredictionUpdates(
-                MODEL_EXECUTOR, targets -> {
-                    if (mWidgetsRecommendationState.setTargets(targets)) {
-                        // No diff, skip
-                        return;
+                MODEL_EXECUTOR, new AppPredictor.Callback() {
+                    @Keep
+                    @Override
+                    public void onTargetsAvailable(@NonNull List<AppTarget> targets) {
+                        if (mWidgetsRecommendationState.setTargets(targets)) {
+                            // No diff, skip
+                            return;
+                        }
+                        mApp.getModel().enqueueModelUpdateTask(
+                                new WidgetsPredictionUpdateTask(mWidgetsRecommendationState, targets));
                     }
-                    mApp.getModel().enqueueModelUpdateTask(
-                            new WidgetsPredictionUpdateTask(mWidgetsRecommendationState, targets));
                 });
         mWidgetsRecommendationState.predictor.requestPredictionUpdate();
     }
