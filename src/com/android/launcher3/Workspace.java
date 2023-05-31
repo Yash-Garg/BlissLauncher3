9d01a36d62e8cd2d4426a58c19dab62c914ebc51
TheScarastic <warabhishek@e.email>
2023-05-31 14:48:01 +0530

    fix(qsb): remove padding only from -1 screen

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -721,7 +721,11 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
 
         mWorkspaceScreens.put(screenId, newScreen);
         mScreenOrder.add(insertIndex, screenId);
-        addView(newScreen, insertIndex);
+        if (screenId == FIRST_SCREEN_ID && FeatureFlags.QSB_ON_FIRST_SCREEN) {
+            addFullScreenPage(newScreen, insertIndex);
+        } else {
+            addView(newScreen, insertIndex);
+        }
         mStateTransitionAnimation.applyChildState(
                 mLauncher.getStateManager().getState(), newScreen, insertIndex);
 
@@ -1299,8 +1303,10 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
 
         getHotseat().setForcedTranslationY(dockTranslationY);
         ((PageIndicatorDots) getPageIndicator()).setForcedTranslationY(dockTranslationY);
-        setPadding(getPaddingLeft(), getPaddingTop(), getPaddingRight(),
-                progress != 0 ? (int) qsbPadding : getPaddingBottom());
+        CellLayout firstScreen = mWorkspaceScreens.get(FIRST_SCREEN_ID);
+        firstScreen.setPadding(
+                firstScreen.getPaddingLeft(), firstScreen.getPaddingTop(), firstScreen.getPaddingRight(),
+                progress != 0 ? (int) qsbPadding : firstScreen.getPaddingBottom());
 
         if (getCurrentPage() != 0) {
             mLauncher.mBlurLayer.setAlpha(0f);
