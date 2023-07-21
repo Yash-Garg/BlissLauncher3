27f1ddbb018f39ed6ae3dd5899e4040e43cd3b7a
Yash Garg <yash.garg.ext@murena.com>
2023-07-21 14:42:52 +0530

    feat(animation): disable spring icon animation in single layer mode

--- src/com/android/launcher3/dragndrop/DragView.java
+++ src/com/android/launcher3/dragndrop/DragView.java
@@ -66,6 +66,8 @@ import com.android.launcher3.util.RunnableList;
 import com.android.launcher3.views.ActivityContext;
 import com.android.launcher3.views.BaseDragLayer;
 
+import foundation.e.bliss.multimode.MultiModeController;
+
 /** A custom view for rendering an icon, folder, shortcut or widget during drag-n-drop. */
 public abstract class DragView<T extends Context & ActivityContext> extends FrameLayout {
 
@@ -218,6 +220,8 @@ public abstract class DragView<T extends Context & ActivityContext> extends Fram
      */
     @TargetApi(Build.VERSION_CODES.O)
     public void setItemInfo(final ItemInfo info) {
+        if (MultiModeController.isSingleLayerMode()) return;
+
         // Load the adaptive icon on a background thread and add the view in ui thread.
         MODEL_EXECUTOR.getHandler().postAtFrontOfQueue(() -> {
             Object[] outObj = new Object[1];
