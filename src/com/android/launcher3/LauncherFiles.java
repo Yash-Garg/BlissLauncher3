35e2f50a25f72e4afe709caed16d0c9268d87ed7
Yash Garg <yash.garg.ext@murena.com>
2023-11-16 09:44:37 +0530

    feat: use correct db for grid 4x6

--- src/com/android/launcher3/LauncherFiles.java
+++ src/com/android/launcher3/LauncherFiles.java
@@ -20,6 +20,7 @@ public class LauncherFiles {
     public static final String LAUNCHER_6_BY_5_DB = "launcher_6_by_5.db";
     public static final String LAUNCHER_5_BY_7_DB = "launcher_5_by_7.db";
     public static final String LAUNCHER_5_BY_6_DB = "launcher_5_by_6.db";
+    public static final String LAUNCHER_4_BY_6_DB = "launcher_4_by_6.db";
     public static final String LAUNCHER_4_BY_5_DB = "launcher_4_by_5.db";
     public static final String LAUNCHER_4_BY_4_DB = "launcher_4_by_4.db";
     public static final String LAUNCHER_3_BY_3_DB = "launcher_3_by_3.db";
@@ -40,6 +41,7 @@ public class LauncherFiles {
             LAUNCHER_6_BY_5_DB,
             LAUNCHER_5_BY_7_DB,
             LAUNCHER_5_BY_6_DB,
+            LAUNCHER_4_BY_6_DB,
             LAUNCHER_4_BY_5_DB,
             LAUNCHER_4_BY_4_DB,
             LAUNCHER_3_BY_3_DB,
