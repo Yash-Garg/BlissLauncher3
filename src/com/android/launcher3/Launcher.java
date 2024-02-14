b214de7f4dc855d8e186b66e61318cbf354d1306
TheScarastic <warabhishek@e.email>
2024-02-14 16:21:05 +0530

    fix(reload): Only request permission if not granted already

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -564,8 +564,11 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
             mLauncherCallbacks.onCreate(savedInstanceState);
         }
 
-        requestPermissions(new String[]{Manifest.permission.MANAGE_EXTERNAL_STORAGE},
-                STORAGE_PERMISSION_REQUEST_CODE);
+        if (checkSelfPermission(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.MANAGE_EXTERNAL_STORAGE},
+                    STORAGE_PERMISSION_REQUEST_CODE);
+        }
 
         mOverlayManager = getDefaultOverlay();
         PluginManagerWrapper.INSTANCE.get(this).addPluginListener(this,
