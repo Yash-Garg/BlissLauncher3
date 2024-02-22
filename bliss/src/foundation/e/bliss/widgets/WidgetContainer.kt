9c0085b14eb6ce89099797e4f2873dff95995d99
TheScarastic <warabhishek@e.email>
2024-02-22 12:41:05 +0530

    fix(widget): Host not listening on initial boot

--- bliss/src/foundation/e/bliss/widgets/WidgetContainer.kt
+++ bliss/src/foundation/e/bliss/widgets/WidgetContainer.kt
@@ -195,15 +195,18 @@ class WidgetContainer(context: Context, attrs: AttributeSet?) :
                     tag = "wrapper_children"
                     orientation = LinearLayout.VERTICAL
                 }
-
             if (isQsbEnabled) {
-                mWidgetHost.startListening()
                 loadWidgets()
             }
+
             return mWrapper
         }
 
         override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+            if (isQsbEnabled) {
+                mWidgetHost.startListening()
+            }
+
             super.onViewCreated(view, savedInstanceState)
         }
 
@@ -224,7 +227,6 @@ class WidgetContainer(context: Context, attrs: AttributeSet?) :
                             Logger.e(TAG, "Could not add widget ${it.flattenToString()}")
                         }
                     }
-                    rebindWidgets()
                 } else {
                     rebindWidgets(true)
                 }
